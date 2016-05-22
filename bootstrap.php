0d27b5b9e37eb2a67d8865f3ba8d102ca79d991a
Tự Kỷ Ích Kỷ <vuthaihoc.ttud@gmail.com>
2016-05-22 22:09:34 +0700

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,19 +1,24 @@
-<?php namespace Matpompili\Imgur\Upload;
-
-/*
-* This file is part of imgur-upload.
-*
-* (c) Matteo Pompili <matpompili@gmail.com>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+<?php
 
 use Flarum\Event\ConfigureClientView;
+use Flarum\Event\DiscussionWillBeSaved;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return function(Dispatcher $events) {
     $events->listen(ConfigureClientView::class, function(ConfigureClientView $event){
         $event->addAssets(__DIR__.'/js/lib/string.js');
+    });
+
+    $events->listen(DiscussionWillBeSaved::class, function(DiscussionWillBeSaved $event){
+
+        $text = $event->discussion->title;
+
+//        $slugtify = new \Cocur\Slugify\Slugify();
+//        $text = $slugtify->slugify($text);
+
+        $text = \VuThaiHoc\FlarumVnSlug\Utils::genSlug($text);
+
+        $event->discussion->slug = $text;
+
     });
 };
